364b812f3539d34581f28c82a1d6e6e82685ebbf
TenteEEEE <wander0055@gmail.com>
2018-12-23 22:18:45 +0900

    anna converter 1.0 & modify mask directory

--- convert_anna.py
+++ convert_anna.py
@@ -9,16 +9,17 @@ panties = os.listdir('./dream/')
 fname =  input("Type pantie name: ./dream/")
 if fname in panties:
     pantie = io.imread('./dream/'+fname)
+    mask = io.imread('./mask/mask_anna.png')
+    pantie = np.bitwise_and(pantie,mask)
     io.imshow(pantie)
     [r,c,d] = pantie.shape
-    # move from hip to front
+
     patch = np.copy(pantie[-170:,546:,:])
     pantie[-100:,546:,:] = 0
-    io.imshow(pantie)
     patch = skt.resize(patch[::-1,::-1,:],(patch.shape[0],40),anti_aliasing=True,mode='reflect')
     [pr,pc,d] = patch.shape
     pantie[157:157+pr,:pc,:] = np.uint8(patch*255)
-    pantie = pantie[:,6:-50,:]
+    pantie = pantie[:,6:-35,:]
 
     # Afine transform matrix
     src_cols = np.linspace(0, c, 10)
@@ -32,14 +33,13 @@ if fname in panties:
     shifter_row = np.convolve(shifter_row,np.ones(30)/30,mode='same')
     dst_rows = src[:, 1] + shifter_row -20
     dst_cols = src[:, 0] + shifter_col
-    # plt.plot((np.sin(np.linspace(0, 1 * np.pi, src.shape[0]))[-30:]*150))
     dst = np.vstack([dst_cols, dst_rows]).T
     affin = skt.PiecewiseAffineTransform()
     affin.estimate(src,dst)
     pantie = np.uint8(skt.warp(pantie, affin)*255)
     
     # Finalize
-    pantie = skt.resize(pantie,(np.int(pantie.shape[0]*1.25),np.int(pantie.shape[1]*1.60)),anti_aliasing=True,mode='reflect')
+    pantie = skt.resize(pantie,(np.int(pantie.shape[0]*1.25),np.int(pantie.shape[1]*1.55)),anti_aliasing=True,mode='reflect')
     io.imsave('anna_pantie.png',np.uint8(pantie*255))
 else:
     print("Cannot find it")
