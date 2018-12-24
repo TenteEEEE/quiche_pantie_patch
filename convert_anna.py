3ac6cfa78705871028b098875c5cfb738a4441b4
TenteEEEE <wander0055@gmail.com>
2018-12-24 22:09:28 +0900

    finalize anna converter

--- convert_anna.py
+++ convert_anna.py
@@ -2,6 +2,7 @@ import os
 import sys
 import skimage.io as io
 import skimage.transform as skt
+import skimage.morphology as skm
 import numpy as np
 
 args = sys.argv
@@ -11,17 +12,14 @@ if fname in panties:
     pantie = io.imread('./dream/'+fname)
     mask = io.imread('./mask/mask_anna.png')
     pantie = np.bitwise_and(pantie,mask)
-    io.imshow(pantie)
     [r,c,d] = pantie.shape
-
     patch = np.copy(pantie[-170:,546:,:])
     pantie[-100:,546:,:] = 0
     patch = skt.resize(patch[::-1,::-1,:],(patch.shape[0],40),anti_aliasing=True,mode='reflect')
     [pr,pc,d] = patch.shape
     pantie[157:157+pr,:pc,:] = np.uint8(patch*255)
-    pantie = pantie[:,6:-35,:]
 
-    # Afine transform matrix
+    # Affine transform matrix
     src_cols = np.linspace(0, c, 10)
     src_rows = np.linspace(0, r, 10)
     src_rows, src_cols = np.meshgrid(src_rows, src_cols)
@@ -37,7 +35,8 @@ if fname in panties:
     affin = skt.PiecewiseAffineTransform()
     affin.estimate(src,dst)
     pantie = np.uint8(skt.warp(pantie, affin)*255)
-    
+    pantie = pantie[:,6:-35,:]
+
     # Finalize
     pantie = skt.resize(pantie,(np.int(pantie.shape[0]*1.25),np.int(pantie.shape[1]*1.55)),anti_aliasing=True,mode='reflect')
     io.imsave('anna_pantie.png',np.uint8(pantie*255))
