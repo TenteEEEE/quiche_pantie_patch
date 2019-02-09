2336818701677766468e731f7415369086e7e021
TenteEEEE <wander0055@gmail.com>
2019-02-09 20:53:07 +0900

    support sign in milk patcher

--- patch_milk.py
+++ patch_milk.py
@@ -23,9 +23,10 @@ if fname in panties:
     cvt.convert2milk(fname)
     pantie = Image.open('./milk_pantie.png')
     origin = Image.open('body_milk.png')
-    # if add_sign:
-    #     sign = Image.open('./material/anna_sign.png')
-    #     origin.paste(sign,(37,861),sign)
+    if add_sign:
+        sign = Image.open('./material/anna_sign.png')
+        sign = sign.resize((int(sign.width * 0.6), int(sign.height * 0.6)))
+        origin.paste(sign,(754,113),sign)
     origin.paste(pantie,(741,224),pantie)
     origin.save('patched_milk.png')
     print("Done. Please check patched.png.")
