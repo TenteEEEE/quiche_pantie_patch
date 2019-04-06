736f8e97105718606b13b1ff281ff9087060faf3
TenteEEEE <wander0055@gmail.com>
2019-04-06 17:02:53 +0900

    lua converter V1.0

--- convert_lua.py
+++ convert_lua.py
@@ -7,83 +7,46 @@ import skimage.morphology as skm
 import numpy as np
 from myutil import *
 
-def affine_transform(img,mx,my,inv=False):
-    [r,c,d] = img.shape
-    src_cols = np.linspace(0, c, 10)
-    src_rows = np.linspace(0, r, 10)
-    src_rows, src_cols = np.meshgrid(src_rows, src_cols)
-    src = np.dstack([src_cols.flat, src_rows.flat])[0]
-    shifter_row = np.zeros(src.shape[0])
-    shifter_col = np.zeros(src.shape[0])
-    if inv:
-        line = np.linspace(np.pi, np.pi/2, src.shape[0])
-    else:
-        line = np.linspace(np.pi/2, np.pi, src.shape[0])
-    shifter_row = -(np.sin(line)*mx)
-    shifter_col = -(np.sin(line)*my)
-    dst_rows = src[:, 1] + shifter_row
-    dst_cols = src[:, 0] + shifter_col
-    dst = np.vstack([dst_cols, dst_rows]).T
-    affin = skt.PiecewiseAffineTransform()
-    affin.estimate(src,dst)
-    return skt.warp(img, affin)
-
-def convert2milk(fname=None):
+def convert2lua(fname=None):
     panties = os.listdir('./dream/')
     if fname is None:
         fname =  input("Type pantie name: ./dream/")
     if fname in panties:
         pantie = io.imread('./dream/'+fname)
-        mask = io.imread('./mask/mask_milk.png')
+        mask = io.imread('./mask/mask_lua.png')
         pantie = np.bitwise_and(pantie,mask)
-        [r,c,d] = pantie.shape
-        front = pantie[:160+30,:200,:]
-        back = pantie[:300,200:,]
-        patch = pantie[-100:-5,546:,:][::-1,::-1,:]
-
-        # Front and front patch pre-processing
-        front = resize(front,(2,2))
-        patch = resize(patch,(1.0,1.15))
-        [fr,fc,_] = front.shape
-        [pr,pf,_] = patch.shape
-        patch_pad = np.zeros((fr,fc,d))
-        patch_pad[-pr:,:pf,:] = patch
-        patch_pad = perspective_transform(patch_pad, np.matrix('1, 0, 0; 0, 1, 0; -0.002,0,1'))
-        patch_pad = patch_pad[-pr-40:,:pf-20,:][:,::-1,:]
-        [pr,pf,_] = patch_pad.shape
-
-        # Alpha blending and transform between front and front patch
-        remain = front[-pr:,:pf,:]*np.float32(skm.dilation(patch_pad[:,:,-1]==0))[:,:,np.newaxis]
-        nonzeromask = np.logical_or(skm.dilation(patch_pad[:,:,-1]==1),remain[:,:,-1]==1)
-        patch_pad = remain + patch_pad
-        normalizer = patch_pad[:,:,-1][:,:,np.newaxis]
-        normalizer[normalizer==0] = 1
-        patch_pad = patch_pad/normalizer
-        patch_pad[:,:,-1] = np.float32(nonzeromask)
-        front[-pr:,:pf,:] = patch_pad
-        front = perspective_transform(front, np.matrix('1, 0, 0; 0, 1, 0; -0.001,0,1'))
-        front = front[:,:-120,:]
-        front = affine_transform(front,30,0,inv=True)
-
-        # Back transform
-        back = resize(back,(1.3,1.3))[:,::-1,:]
-        back = perspective_transform(back, np.matrix('1, 0, 0; 0, 1, 0; 0.0002,0,1'))[:,::-1,:]
-        back = affine_transform(back,70,150,inv=False)
-        back = back[:,138:,:]
+        
+        patch = np.copy(pantie[-140:-5,546:,:])
+        pantie[-115:,546:,:] = 0
+        patch = skt.resize(patch[::-1,::-1,:],(patch.shape[0],65),anti_aliasing=True,mode='reflect')
+        [pr,pc,d] = patch.shape
+        pantie[127-5:127-5+pr,:pc,:] = np.uint8(patch*255)
+        
+        front = pantie[:260,:300,:]
+        front = np.pad(front,[(0,0),(100,130),(0,0)],mode='constant')    
+        front = perspective_transform(front,np.matrix('1, 0.12, 0; 0, 1, 0; 0.0005,0,1'))
+        front = perspective_transform(front,np.matrix('1, 0.0, 0; 0, 1, 0; -0.0009,0,1'))
+        front = front[:-45,70:335]
+        front = resize(front,[2,2.44])[:,40:,:]
+        
+        back = pantie[:265,300:,:]
+        back = np.pad(back,[(15,0),(0,200),(0,0)],mode='constant')
+        back = affine_transform(back,-10,150,0,0,inv=True)
+        back = perspective_transform(back, np.matrix('1, -0.2, 0; 0, 1, 0; -0.0001,0,1'))
+        back = back[2:-24,25:-5-17,:]
+        back = resize(back,[1.71,1.79])
 
         [fr,fc,_] = front.shape
         [br,bc,_] = back.shape
-        pantie = np.zeros((np.max([fr,br]),fc+bc-2,d))
-        shiftr = 35
-        row_point = np.clip(shiftr+fr,0,np.max([fr,br]))
-        pantie[shiftr:row_point,:fc,:] = front[:-(shiftr+fr-row_point),:,:]
-        pantie[:bc,fc-1:,:] = back[:,1:,:]
-        # io.imshow(pantie)
+        shift_y = 20
+        pantie = np.zeros((np.max([fr,br])+shift_y,fc+bc,d))
+        pantie[shift_y:shift_y+fr,:fc,:] = front
+        pantie[:br,fc:,:] = back
 
         # Finalize
-        io.imsave('milk_pantie.png',np.uint8(pantie*255))
+        io.imsave('lua_pantie.png',pantie)
     else:
         print("Cannot find it")
         
 if __name__ == '__main__':
-    convert2milk()
+    convert2lua()
--- myutil.py
+++ myutil.py
@@ -10,7 +10,7 @@ def perspective_transform(img,matrix):
 def resize(img,mag):
     return skt.resize(img,(np.int(img.shape[0]*mag[0]),np.int(img.shape[1]*mag[1])),anti_aliasing=True,mode='reflect')
 
-def affine_transform(img,mx,my,inv=False):
+def affine_transform(img,mx,my,phix=0,phiy=0,inv=False):
     [r,c,d] = img.shape
     src_cols = np.linspace(0, c, 10)
     src_rows = np.linspace(0, r, 10)
@@ -19,11 +19,13 @@ def affine_transform(img,mx,my,inv=False):
     shifter_row = np.zeros(src.shape[0])
     shifter_col = np.zeros(src.shape[0])
     if inv:
-        line = np.linspace(np.pi, np.pi/2, src.shape[0])
+        linex = np.linspace(np.pi+phix, np.pi/2+phix, src.shape[0])
+        liney = np.linspace(np.pi+phiy, np.pi/2+phiy, src.shape[0])
     else:
-        line = np.linspace(np.pi/2, np.pi, src.shape[0])
-    shifter_row = -(np.sin(line)*mx)
-    shifter_col = -(np.sin(line)*my)
+        linex = np.linspace(np.pi/2+phix, np.pi+phix, src.shape[0])
+        liney = np.linspace(np.pi/2+phiy, np.pi+phiy, src.shape[0])
+    shifter_row = -(np.sin(linex)*mx)
+    shifter_col = -(np.sin(liney)*my)
     dst_rows = src[:, 1] + shifter_row
     dst_cols = src[:, 0] + shifter_col
     dst = np.vstack([dst_cols, dst_rows]).T
