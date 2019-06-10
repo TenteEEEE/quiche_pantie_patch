331aae158c5d93f6c4d27f03ffbe10ba3bce44f8
TenteEEEE <wander0055@gmail.com>
2019-06-09 20:08:30 -0400

    shading and base color painting

--- convert_bra.py
+++ convert_bra.py
@@ -10,20 +10,35 @@ panties = os.listdir('./dream/')
 bra_mask = io.imread('./mask/bra.png')[:430,1024:1024+620,:]
 bra_center = io.imread('./mask/bra_center.png')[:430,1024:1024+620,:]
 bra_shade = io.imread('./material/bra_shade.png')[:430,1024:1024+620,:]
-# plt.imshow(bra_shade)
+[r,c,d] = bra_mask.shape
 
 # fname = panties[59]
 fname = panties[100]
 pantie = io.imread('./dream/'+fname)
-hip = pantie[20:180,-130:]
-hip = skt.resize(hip,(np.int(hip.shape[0]*1.6),np.int(hip.shape[1]*1.6)),anti_aliasing=True,mode='reflect')
 
-[r,c,d] = bra_mask.shape
-[hr,hc,hd] = hip.shape
+# center painting
+center_texture = pantie[20:180,-130:]
+center_texture = skt.resize(center_texture,(np.int(center_texture.shape[0]*1.6),np.int(center_texture.shape[1]*1.6)),anti_aliasing=True,mode='reflect')
+[hr,hc,hd] = center_texture.shape
 posx = 20
 posy = 170
 padx = c-hc-posx
 pady = r-hr-posy
-hip = (np.pad(hip,[(posy,pady),(posx,padx),(0,0)],mode='constant')*255).astype(np.uint8)
-hip[:,:,3] = bra_center[:,:,0]
-plt.imshow(hip)
+center_texture = (np.pad(center_texture,[(posy,pady),(posx,padx),(0,0)],mode='constant'))
+center_texture[:,:,3] = bra_center[:,:,0]
+
+# base color painting
+front = pantie[20:100,30:80,:]
+# base = np.mean(np.mean(front,axis=0),axis=0)
+base = np.median(np.median(front,axis=0),axis=0)/255.0
+base_texture = np.copy(bra_mask).astype(np.float32)/255.0
+base_texture = (base_texture*base)
+
+# convined them and shading
+center_mask = (bra_center[:,:,0][:,:,None]/255.0).astype(np.float32)
+convined_texture = base_texture*(1-center_mask) + center_texture*center_mask
+convined_texture[:,:,:3] *= bra_shade[:,:,:3].astype(np.float32)/255.0
+convined_texture[:,:,3] = (bra_mask[:,:,0]/255.0).astype(np.float32)
+
+# finalize
+convined_texture = (convined_texture*255.0).astype(np.uint8)
