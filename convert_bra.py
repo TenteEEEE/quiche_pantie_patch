d61ec7331f745427e8c655a7867315e6745af6d2
TenteEEEE <wander0055@gmail.com>
2019-06-14 19:35:04 -0400

    bra converter ver0.9

--- convert_bra.py
+++ convert_bra.py
@@ -9,29 +9,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 panties = os.listdir('./dream/')
+ribbon_mask = io.imread('./mask/ribbon.png')
 bra_mask = io.imread('./mask/bra.png')[:430, 1024:1024 + 620, :]
 bra_center = io.imread('./mask/bra_center.png')[:430, 1024:1024 + 620, :]
-bra_shade = io.imread('./material/bra_shade.png')[:430, 1024:1024 + 620, :]
+bra_shade = io.imread('./material/bra_shade.png')[:430, 1024 - 620:1024 + 620, :]
+frill = io.imread('./material/bra_frill.png')[:430, 1024 - 620:1024 + 620, :]
+lace = io.imread('./material/bra_lace.png')[:430, 1024 - 620:1024 + 620, :]
 [r, c, d] = bra_mask.shape
+os.makedirs('./converted/bra/', exist_ok=True)
 
-for fname in panties:
-    # fname = panties[100]
+
+def alpha_brend(ref, template):
+    return ref * (1 - template[:, :, -1][:, :, None]) + template[:, :, :3] * template[:, :, -1][:, :, None]
+
+# flag frill lace ribbon shading texture
+
+for fname in panties[70:]:
     print('Process: ' + fname)
     pantie = io.imread('./dream/' + fname)
 
-    # center painting and extract design of the texture
+    # crop center texture
     center_texture = pantie[20:140, -170:-15]
     center_texture = skt.resize(center_texture, (np.int(center_texture.shape[0] * 1.6), np.int(center_texture.shape[1] * 1.6)), anti_aliasing=True, mode='reflect')
     [hr, hc, hd] = center_texture.shape
-    design = rgb2gray(center_texture[:,:,:3])[::-1,::-1]
-    design = (design-np.min(design))/(np.max(design)-np.min(design))
+    
+    # make seamless design
+    design = rgb2gray(center_texture[:, :, :3])[::-1, ::-1]
+    design = (design - np.min(design)) / (np.max(design) - np.min(design))
     edge = 30
-    design_seamless =  gaussian(design,sigma=3)
-    design_seamless[edge:-edge,edge:-edge] = design[edge:-edge,edge:-edge]
-    y = np.arange(-hr/2,hr/2,dtype=np.int16)
-    x = np.arange(-hc/2,hc/2,dtype=np.int16)    
-    design_seamless = (design_seamless[y,:])[:,x] # rearrange pixels
-    design_seamless = np.tile(design_seamless,(3,3))
+    design_seamless = gaussian(design, sigma=3)
+    design_seamless[edge:-edge, edge:-edge] = design[edge:-edge, edge:-edge]
+    y = np.arange(-hr / 2, hr / 2, dtype=np.int16)
+    x = np.arange(-hc / 2, hc / 2, dtype=np.int16)
+    design_seamless = (design_seamless[y, :])[:, x]  # rearrange pixels
+    design_seamless = np.tile(design_seamless, (3, 3))
+    
+    # paste center texture
     posx = 20
     posy = 230
     padx = c - hc - posx
@@ -54,21 +67,48 @@ for fname in panties:
     shade[:, :, 1] *= -2 + np.mean(base)
     shade[:, :, 2] /= 6 + 3 * np.mean(base)
     shade = hsv2rgb(shade)
-    base_texture[:,:,:3] -= shade[:r,:c,]
+    base_texture[:, :, :3] -= shade[:r, :c, ]
 
-    # convined them and shading
+    # convine center and base textures and shading
     center_mask = (bra_center[:, :, 0][:, :, None] / 255.0).astype(np.float32)
     convined_texture = base_texture * (1 - center_mask) + center_texture * center_mask
+    convined_texture = np.concatenate([convined_texture[:, ::-1, :], convined_texture], axis=1)
     shade = rgb2hsv(np.tile((bra_shade[:, :, 3].astype(np.float32) / 255.0)[:, :, None], [1, 1, 3]) * base_shade)
     shade[:, :, 0] -= 1
-    shade[:, :, 1] *= 1 + np.mean(base)/3
-    shade[:, :, 2] /= 1 + 0.5 * np.mean(base)
+    shade[:, :, 1] *= 0.5 + np.mean(base) / 3
+    shade[:, :, 2] /= 1 + 1 * np.mean(base)
     shade = hsv2rgb(shade)
     convined_texture[:, :, :3] -= shade
-    convined_texture[:, :, 3] = (bra_mask[:, :, 0] / 255.0).astype(np.float32)
-    convined_texture = np.clip(convined_texture, 0.0, 1.0)
+
+    # Paste frill or lace
+    # decoration = np.copy(lace).astype(np.float32) / 255.0
+    decoration = np.copy(frill).astype(np.float32) / 255.0
+    deco_shade = np.median(pantie[5, :, :3], axis=0) / 255.0
+    shade = rgb2hsv(decoration[:, :, :3] * deco_shade)
+    shade[:, :, 1] *= 0.5 + np.mean(base) / 3
+    decoration_shaded = hsv2rgb(shade)
+    decoration_shaded = np.dstack((decoration_shaded, decoration[:, :, 3]))
+    convined_texture[:, :, :3] = alpha_brend(convined_texture[:, :, :3], decoration_shaded)
+
+    # Crop ribbon and pasete <0070.png
+    ribbon = np.copy(pantie)
+    ribbon[:, :, 3] = ribbon_mask[:, :, 1]
+    ribbon = ribbon[19:58, 8:30] / 255.0
+    ribbon_bi = np.concatenate([ribbon[:, ::-1, :], ribbon], axis=1)
+    rib_r = 383
+    rib_c = base_texture.shape[1] - int(ribbon_bi.shape[1] / 2)
+    convined_texture[rib_r:rib_r + ribbon_bi.shape[0], rib_c:rib_c + ribbon_bi.shape[1], :3] = \
+        alpha_brend(convined_texture[rib_r:rib_r + ribbon_bi.shape[0], rib_c:rib_c + ribbon_bi.shape[1], :3], ribbon_bi)
 
     # finalize
+    final_mask = np.copy(bra_mask[:, :, 0]).astype(np.float32) / 255
+    final_mask[rib_r:rib_r + ribbon.shape[0], :ribbon.shape[1]] += ribbon[:, :, 3]
+    final_mask = np.concatenate([final_mask[:, ::-1], final_mask], axis=1)
+    final_mask += decoration[:, :, 3]
+    final_mask = np.clip(final_mask, 0, 1)
+    convined_texture[:, :, 3] = final_mask
+    convined_texture = np.clip(convined_texture, 0.0, 1.0)
     convined_texture = (convined_texture * 255.0).astype(np.uint8)
+
     # plt.imshow(convined_texture)
-    io.imsave('./converted/bra/'+fname, convined_texture)
+    io.imsave('./converted/bra/' + fname, convined_texture)
