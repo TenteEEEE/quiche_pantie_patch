31dc87b191b184320f0c15710122add28ee38e39
TenteEEEE <wander0055@gmail.com>
2020-05-18 18:33:17 +0900

    Necomaid pantie converter Ver. 1.0

--- src/models/necomaid.py
+++ src/models/necomaid.py
@@ -8,30 +8,21 @@ from src.utils.imgproc import *
 
 class patcher(patcher):
     def __init__(self, body='./body/body_necomaid.png', **options):
-        super().__init__('スピカ', body=body, pantie_position=[0, 0], **options)
+        super().__init__('NecoMaid', body=body, pantie_position=[104, 2482], **options)
         self.mask = io.imread('./mask/mask_necomaid.png')
 
     def convert(self, image):
-        image = Image.open('./dream/0101.png')
         pantie = np.array(image)
         patch = np.copy(pantie[-170:, 546:, :])
         pantie[-150:, 546:, :] = 0
-        patch = skt.resize(patch[::-1, ::-1, :], (patch.shape[0]-50, 60), anti_aliasing=True, mode='reflect')
+        patch = skt.resize(patch[::-1, ::-1, :], (patch.shape[0] - 50, 60), anti_aliasing=True, mode='reflect')
         [pr, pc, d] = patch.shape
         pantie[120:120 + pr, :pc, :] = np.uint8(patch * 255)
-        
+
         arrx = np.zeros(100)
         arry = np.zeros(100)
-        arry[10:] += np.sin(np.linspace(0,np.pi,90))*120
-        # arry[3:13] += np.sin(np.linspace(0,np.pi,10))*-50
-        import matplotlib.pyplot as plt
-        pantie_ = affine_transform_by_arr(pantie, arrx, arry)
-        
-        io.imshow(pantie_)
-        io.imsave('test.png', pantie_)
-        
-        # pantie = np.uint8(resize(pantie, [1.38, 1.62]) * 255)[:, 11:]
-        # pantie = np.bitwise_and(pantie, self.mask)[:, 1:]
-        # pantie = np.concatenate((pantie[:, ::-1], pantie), axis=1)
-        # components = self.gen_components(image)
+        arry[10:] += np.sin(np.linspace(0, np.pi, 90)) * 300
+        pantie = affine_transform_by_arr(pantie, arrx, arry)
+        pantie = np.uint8(resize(pantie[:130, :330], [3.21, 3.21]) * 255)
+        pantie = np.bitwise_and(pantie, self.mask)[4:]
         return Image.fromarray(pantie)
